48c4a8eb8edc925cf12e734ef1c5d29d0d5bce2a
ArkadiuszPucek <arkadiusz.pucek@gmail.com>
2023-09-18 11:31:38 +0200

    viaplay - first commit

---
+++ src/main/java/pl/puccini/viaplay/ViaplayApplication.java
@@ -0,0 +1,13 @@
+package pl.puccini.viaplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViaplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/puccini/viaplay/ViaplayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.puccini.viaplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
